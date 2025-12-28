9bdda0352a8aeca79e9ff1d895b8be403e58be9c
ㅤ <132016691+rottentanx@users.noreply.github.com>
2025-12-28 19:34:36 -0300

    Refactor IMC calculator to use input fields
    Updated the startCalculator function to retrieve values from input fields and added a hideForm function to hide the IMC form after calculations.

--- src/js/script.js
+++ src/js/script.js
@@ -5,21 +5,28 @@
         function showButton(){
             document.querySelector("button[onclick='refreshPage()']").style.display = "inline-block";
                 document.querySelector("button[onclick='startCalculator()']").style.display = "none";
-        }
+        } // Shows the "Try Again" button and hides the "Calculate" button.
+        
+        function hideForm(){
+            document.getElementById("imcForm").style.display="none";
+
+        } // Hides the form after calculations are done.
 
         function startCalculator() {
-            var peso = Number(prompt("Por favor insira seu peso em kilogramas:").replace(",", "."));
-            var altura = Number(prompt("Por favor insira sua altura em metros:").replace(",", "."));
+            var peso = Number(document.getElementById("weight").value.replace(",", "."));
+            var altura = Number(document.getElementById("height").value.replace(",", "."));
+            showButton();
+                hideForm();
             // Verifica se os valores inseridos são válidos
             if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) { 
                 alert("Por favor insira valores válidos para o calculo.");
                 document.getElementById("invalidValue").style.display = "block";
                 document.getElementById("validValue").style.display = "none";
+                
                 // Toca a música de fundo para valores inválidos
                 var audio2 = document.getElementById("backgroundMusic2");
                 audio2.play();
-                showButton();
-                
+
             }
             // Se os valores forem válidos, calcula o IMC e exibe o resultado
             else {
@@ -30,10 +37,10 @@
                 audio.play();
                 document.getElementById("invalidValue").style.display = "none";
                 document.getElementById("validValue").style.display = "block";
-                showButton();
+                
             }
         }
         
         function refreshPage(){
             window.location.reload();
-        }
+        } // Refreshes the page to allow a new calculation.
